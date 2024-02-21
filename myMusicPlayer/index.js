e5f4f13fe0eda17a2f301167a9cda4ebedce75d4
sergeistetsko <sergeistetsko2021@yandex.by>
2024-02-21 19:54:53 +0300

    add playlistData

---
+++ myMusicPlayer/index.js
@@ -0,0 +1,76 @@
+const playlistData = [
+    {
+        id: 1,
+        coverPlaylistUrl: './assets/images/mainCoverImage1.png',
+        topTrackUrl: './assets/icons/is-trend.png',
+        numberOfTracks: 0,
+        totalTracksDurationInSeconds: 0,
+        tracksInformation: [
+            {
+                trackCoverUrl: 'trackCoverImage1Playlist1.png',
+                isTopTrack: false,
+                artistName: 'Eminem',
+                trackName: 'Rap God',
+                musicFileUrl: './assets/audio/eminem-rap-god.mp3',
+            },
+            {
+                trackCoverUrl: 'trackCoverImage1Playlist2.png',
+                isTopTrack: false,
+                artistName: '50 cent',
+                trackName: 'In da Club',
+                musicFileUrl: './assets/audio/50сent-in-da-club.mp3',
+            },
+            {
+                trackCoverUrl: 'trackCoverImage1Playlist3.png',
+                isTopTrack: false,
+                artistName: 'DMX',
+                trackName: 'X Gon\' Give It To Ya',
+                musicFileUrl: './assets/audio/dmx-x-gon-give-it-to-ya.mp3',
+            },
+            {
+                trackCoverUrl: 'trackCoverImage1Playlist4.png',
+                isTopTrack: false,
+                artistName: 'Eminem feat 50 Cent, Cashis, Lloyd Banks',
+                trackName: 'You Don\'t Know',
+                musicFileUrl: './assets/audio/eminem-50cent-you-dont-know.mp3',
+            },
+        ]
+    },
+    {
+        id: 2,
+        coverPlaylistUrl: './assets/images/mainCoverImage2.png',
+        topTrackUrl: './assets/icons/is-trend.png',
+        numberOfTracks: 0,
+        totalTracksDurationInSeconds: 0,
+        tracksInformation: [
+            {
+                trackCoverUrl: 'trackCoverImage1Playlist2.png',
+                isTopTrack: false,
+                artistName: 'Public Enemy',
+                trackName: 'Fight the Power',
+                musicFileUrl: './assets/audio/public-enemy-fight-the-power.mp3',
+            },
+            {
+                trackCoverUrl: 'trackCoverImage2Playlist2.png',
+                isTopTrack: false,
+                artistName: 'Vanila Ice',
+                trackName: 'Ice Ice Baby',
+                musicFileUrl: './assets/audio/vanilla-ice-ice-ice-baby.mp3',
+            },
+            {
+                trackCoverUrl: 'trackCoverImage3Playlist2.png',
+                isTopTrack: false,
+                artistName: 'MC Hammer',
+                trackName: 'You Can’t Touch This',
+                musicFileUrl: './assets/audio/mc-hammer-u-can\'t-touch-this.mp3',
+            },
+            {
+                trackCoverUrl: 'trackCoverImage4Playlist2.png',
+                isTopTrack: false,
+                artistName: 'Brand Nubian',
+                trackName: 'Brand Nubian',
+                musicFileUrl: './assets/audio/brand-nubian-brand-nubian.mp3',
+            },
+        ]
+    },
+]
